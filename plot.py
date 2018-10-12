9d73d4fbafe4884d2c28cd26a937e8d6101a837a
vikram.g <vikram.g068@gmail.com>
2018-10-12 17:46:54 +0530

    Update plot.py

--- plot.py
+++ plot.py
@@ -10,8 +10,7 @@ def textogram(points_label,values,labels):
     plt.axis([0,len(labels) + 1,0, max(values)+1])
     points=list(range(1, len(points_label) + 1))
     plt.xticks(points,points_label)
-    fig_size=plt.rcParams["figure.figsize"]
-    plt.rcParams["figure.figsize"] =[6.4,fig_size[1]]
+    plt.rcParams["figure.figsize"] =[6.4,4.8]
     yscale=89/(2*(max(values)+1)) #factor by which pixel related to font_size
     for index,text in zip(points,labels):
         plt.text(index,0,text,rotation=90,va = "bottom",ha = "center",fontsize = 10*values[index-1]*(yscale/len(text)),family='monospace',color=(random.uniform(0, 1),random.uniform(0, 1),random.uniform(0, 1)))
