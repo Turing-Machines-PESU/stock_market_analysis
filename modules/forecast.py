8c16b2c61c2a75c596240af1ed633b96f68c4bf6
craterkamath <vinayakkamath2010@gmail.com>
2018-11-20 22:17:50 +0530

    Forecast: Testing and Debugging.

--- modules/forecast.py
+++ modules/forecast.py
@@ -17,6 +17,12 @@ from fbprophet import Prophet
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
 import pandas as pd
 
+from keras.models import Sequential
+from keras.layers.recurrent import LSTM
+from keras.layers.core import Dense, Activation, Dropout
+from sklearn.metrics import mean_squared_error
+from sklearn.utils import shuffle
+
 from basic import rmse
 
 # -----------  Forecasting Models  -----------
@@ -31,8 +37,6 @@ def auto_arimax(X, test_split = 0.2):
     train_data, test_data = X[:-test_samples], X[-test_samples:]
     train_data.columns = ["Training Data"]
     test_data.columns = ["Test Data"]
-    train_data.rename("Train")
-    train_data.rename("Test")
 
     stepwise_model = auto_arima(X, start_p=1, start_q=1,max_p=3, max_q=3, m=12,start_P=0, seasonal=True,d=1, D=1, trace=True,error_action='ignore',suppress_warnings=True,stepwise=True)
     stepwise_model.fit(train_data)
@@ -42,15 +46,47 @@ def auto_arimax(X, test_split = 0.2):
     result = pd.concat([train_data, test_data, predictions], axis = 1)
     result.plot()
     
-    return rmse(np.array(test_data), np.array(predictions).flatten())
+    return rmse(np.array(test_data).flatten(), np.array(predictions).flatten())
 
 
-def shallow_lstm(X, test_split = 0.2):
-	X['Item'] = pd.Series(list(range(len(X))))
-	test_samples = int(X.shape[0] * test_split)
-	train_data, test_data = X[:-test_samples], X[-test_samples:]
+def shallow_lstm(X):
 
-	# Yet to implement
+	store_data = X
+	x_test = X["2017"]
+	X = X["2016"]
+	look_back = 1
+
+	def create_dataset(dataset, look_back=1):
+		dataX, dataY = [], []
+		for i in range(len(dataset)-look_back-1):
+			a = dataset[i:(i+look_back)]
+			dataX.append(a)
+			dataY.append(dataset[i + look_back])
+		return np.array(dataX), np.array(dataY)
+
+	data = X.Close.values
+	x, y = create_dataset(data)
+	x_t, y_t = create_dataset(x_test.Close.values)
+
+	trainX = np.reshape(x, (x.shape[0], 1, x.shape[1]))
+	testX = np.reshape(x_t, (x_t.shape[0], 1, x_t.shape[1]))
+
+	model = Sequential()
+	model.add(LSTM(4, input_shape=(1, look_back)))
+	model.add(Dense(1))
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	model.fit(trainX, y, epochs=50, batch_size=1, verbose=2)
+
+	trainPredict = model.predict(trainX)
+	testPredict = model.predict(testX)
+	# print(testPredict)
+    
+	predictions = pd.DataFrame(testPredict, index = x_test.index[:-2])
+	results = pd.concat([store_data, predictions], axis = 1)
+	results.columns = ["Train + Test", "Predictions"]    
+	results.plot()
+	
+	return rmse(np.array(testX).flatten(), np.array(testPredict).flatten())
 
 
 def prophet(X):
@@ -87,7 +123,7 @@ def prophet(X):
 
 def regression_model(X):
 	# Send data frame with only one column "Date" and the index set as date.
-	# Date from 2016 to the end of the dataset
+	# Date from 2016 to the end of the datasets
 	data = X.reset_index()
 	data['Date'] = data['Date'].astype('datetime64[ns]')
 	data.set_index("Date", inplace=True)
@@ -105,6 +141,7 @@ def regression_model(X):
 	merged = pd.concat([train_data.Close, test_data.Close, predictions], axis = 1)
 	merged.columns = ["Train Data", "Test Data", "Fitted Values"]
 	merged.plot()
+	return rmse(np.array(test_data.Close), np.array(predictions[-len(test_data):]).flatten())
 
 # data = pd.read_csv("../Stocks/goog.us.txt")
 # data['Date'] = data['Date'].astype('datetime64[ns]')
